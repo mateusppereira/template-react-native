fe7751468585f612f2a816dd33843cbeadd13199
Mateus Andrade Pereira <mateus.pereira@agrotools.local>
2019-05-31 16:30:07 -0300

    appstate

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import { AppRegistry } from 'react-native';
-import App from './src';
+import { AppRegistry, AppState } from 'react-native';
 import { name as appName } from './app.json';
+import App from './src';
+
+AppState.addEventListener('change', () => {})
 
 AppRegistry.registerComponent(appName, () => App);
